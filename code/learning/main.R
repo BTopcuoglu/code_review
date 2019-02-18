02722645d320c5506eab308169fa11c0c12735fc
BegumTop <begumtop@umich.edu>
2019-02-17 22:41:14 -0500

    add tidyverse

--- code/learning/main.R
+++ code/learning/main.R
@@ -34,7 +34,7 @@
 
 ################### IMPORT LIBRARIES and FUNCTIONS ###################
 # The dependinces for this script are consolidated in the first part
-deps = c("caret" ,"rpart", "xgboost", "randomForest", "kernlab","LiblineaR", "pROC");
+deps = c("tidyverse", "caret" ,"rpart", "xgboost", "randomForest", "kernlab","LiblineaR", "pROC");
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE, repos = "http://cran.us.r-project.org", dependencies=TRUE);
